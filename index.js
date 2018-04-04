a510629d744bd47e6677801dce27976fa4059212
Mark Fisher <mkfisher82@gmail.com>
2018-04-05 05:32:05 +1200

    App completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/:id', (req, res) => {
+    let unix = parseInt(req.params.id);
+    let natural;
+
+    if (Number.isInteger(unix)) {
+        // console.log('Given unix time');
+        natural = new Date(req.params.id * 1000).toDateString(); //Change unix to natural
+    } else {
+        // console.log('Given natural date');
+        natural = new Date(req.params.id);
+        unix = natural.valueOf() / 1000; //change natural to unix
+    }
+
+    res.json({"unix": unix, "natural": natural});
+});
+
+app.listen(3000, () => console.log('App listening on port 3000'));
