31c22bd34b6e211f283182da362e04d94087ff5e
Mark Fisher <mkfisher82@gmail.com>
2018-04-12 08:30:22 +1200

    Put the code through eslint and improved date display

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send("Welcome to my Free Code Camp timestamp app"));
+app.get('/', (req, res) => res.send('Welcome to my Free Code Camp timestamp app'));
 
 app.get('/:id', (req, res) => {
-    let unix = parseInt(req.params.id);
-    let natural;
+  let unix = parseInt(req.params.id, 10);
+  let natural;
 
-    if (Number.isInteger(unix)) {
-        // console.log('Given unix time');
-        natural = new Date(req.params.id * 1000).toDateString(); //Change unix to natural
-    } else {
-        // console.log('Given natural date');
-        natural = new Date(req.params.id);
-        unix = natural.valueOf() / 1000; //change natural to unix
-    }
+  if (Number.isInteger(unix)) {
+    // console.log('Given unix time');
+    natural = new Date(req.params.id * 1000).toDateString(); // Change unix to natural
+  } else {
+    // console.log('Given natural date');
+    const date = new Date(req.params.id);
+    natural = date.toDateString();
+    unix = date.valueOf() / 1000; // change natural to unix
+  }
 
-    res.json({"unix": unix, "natural": natural});
+  res.json({
+    unix,
+    natural,
+  });
 });
 
-app.listen(port, () => console.log('App listening on port ' + port));
+app.listen(port, () => console.log(`App listening on port ${port}`));
+
+// url https://floating-garden-66926.herokuapp.com/
